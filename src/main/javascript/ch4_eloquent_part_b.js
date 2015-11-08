7f0ded733a2b102bbe8dce4be3e9866ac062b379
Nico de Wet <nico@nicodewet.com>
2015-11-09 08:14:39 +1300

    ch 4, eloquent, arrayToList

--- src/main/javascript/ch4_eloquent_part_b.js
+++ src/main/javascript/ch4_eloquent_part_b.js
@@ -1,13 +1,22 @@
 function arrayToList(array) {
 	var rootListNode = {value: array[0], pointer: null}
+	var previousNode = rootListNode;
 	for (var i = 1; i < array.length; i++) {
 		var newNode = {value: array[i], pointer: null}
-		if (i == 1) {
-			rootListNode.pointer = newNode;
-		}
+		previousNode.pointer = newNode;
+		newNode.value = array[i];
+		previousNode = newNode;
 	}
 	return rootListNode;
 }
+// test
+var array = [1,2,3,4,5]
+var node = arrayToList(array);
+while(node != null) {
+	print(node.value)
+	node = node.pointer
+}
+ 
 function ListToArray() {
 	
 }
